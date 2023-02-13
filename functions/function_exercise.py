82f71d413cc0c7302534f9a7ce7a392dedcb5a95
eMobilis <carmenmose2004@gmail.com>
2023-02-13 08:52:30 +0300

    first function on python

---
+++ functions/function_exercise.py
@@ -0,0 +1,5 @@
+def calculate_months():
+    age = int(input("enter your age"))
+    calc_age = age * 12
+    print(f"you have lived {calc_age} months")
+calculate_months()
---
+++ functions/functions.py
@@ -0,0 +1,9 @@
+def starmachine() :
+    print("do you want to start the machine")
+
+starmachine()
+
+
+def restartmachine():
+    print("do you want to restart machine")
+restartmachine()
